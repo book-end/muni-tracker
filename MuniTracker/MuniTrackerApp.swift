060b3a1ce451b97a208a20670017368e1d33353a
book-end <code.rps@aleeas.com>
2026-03-09 16:32:54 -0700

    Initial Commit

---
+++ MuniTracker/MuniTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuniTrackerApp.swift
+//  MuniTracker
+//
+//  Created by Mabel on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct MuniTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuniTrackerTests/MuniTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuniTrackerTests.swift
+//  MuniTrackerTests
+//
+//  Created by Mabel on 3/9/26.
+//
+
+import Testing
+@testable import MuniTracker
+
+struct MuniTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
