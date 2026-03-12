465bc07e01fa128b4c026a1b39756758ab7ea258
book-end <code.rps@aleeas.com>
2026-03-11 22:12:36 -0700

    Add fetchBusArrivals to fetch real bus data

--- MuniTracker/BusArrivalViewModel.swift
+++ MuniTracker/BusArrivalViewModel.swift
@@ -13,4 +13,19 @@ class BusArrivalViewModel: ObservableObject {
     // BAsically ViewModel owns that data instead of ContentView
     @Published var buses: [BusArrival] = []
     @Published var loadingState: LoadingState = .loading
+    
+    // Function to fetch real bus data
+    // Note: async means to let everything else keep running while this func runs
+    func fetchBusArrivals() async {
+        let apiKey = SecretsManager.apiKey()
+        
+        // Build url string with api key
+        let urlString = "http://api.511.org/transit/StopMonitoring?api_key=\(apiKey)&agency=[operatorID]"
+        
+        // Convert url string into url object
+        guard let urlObject = URL(string: urlString) else {
+            loadingState = .error("Invalid URL")
+            return
+        }
+    }
 }
